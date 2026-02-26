f1a0dac6d19b06c0841ae627004030e5f13bcd24
hxuanyu <2252193204@qq.com>
2026-02-26 12:28:24 +0800

    Add bingpaper-desktop

---
+++ Casks/b/bingpaper-desktop.rb
@@ -0,0 +1,17 @@
+cask "bingpaper-desktop" do
+  version "0.0.4"
+  sha256 "dcfe0bb6c4a7d874bb5acc8f7d987964e726dd5b78ccae74d5bfa304fc7bd5ea"
+
+  url "https://github.com/hanxuanyu/BingPaperDesktop/releases/download/v#{version}/BingPaperDesktop_macos.dmg"
+  name "BingPaperDesktop"
+  desc "Bing daily wallpaper desktop client built with Wails v2, Go, and React"
+  homepage "https://github.com/hanxuanyu/BingPaperDesktop"
+
+  app "BingPaperDesktop.app"
+
+  uninstall quit: "com.hxuanyu.bingpaperdesktop"
+
+  zap trash: [
+    "~/Library/Application Support/BingPaperDesktop",
+  ]
+end
